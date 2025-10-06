79974b38783831334376a70ebe021549892960d3
BURNSIDE HIGH SCHOOL <burntwyrm.work@gmail.com>
2025-10-06 13:00:11 +1300

    Update controller_alpha.ino

--- scripts/controller/controller_alpha/controller_alpha.ino
+++ scripts/controller/controller_alpha/controller_alpha.ino
@@ -1,6 +1,17 @@
+// ESP32
+// Controller Testing ALPHA
+// https://github.com/BurntWywrm/L2-robot-arm
+
+/*
+ALPHA: Tests if the potentiometers
+properly sends signals to the ESP32.
+As well as the ESP32 properly converting
+the signals to the correct angle.
+*/
 
 void setup()
 {
+    Serial.begin(115200); // Starts Serial Monitor with a baud rate of 115200
 }
 
 void loop()
