b0a38c83522d534ac45e0183d8ede062f73176b7
Burnt <burntwyrm.work@gmail.com>
2025-10-06 13:50:13 +1300

    Started scripting controller_beta.ino

---
+++ scripts/controller/controller_beta/controller_beta.ino
@@ -0,0 +1,18 @@
+// ESP32
+// Controller Testing BETA
+// https://github.com/BurntWywrm/L2-robot-arm
+
+/*
+BETA: Tests the ESP-NOW protocol so that
+it can properly send information to the
+main body, the robot arm.
+*/
+
+void setup()
+{
+    Serial.begin(115200); // Starts serial monitor with a baud rate of 115200
+}
+
+void loop()
+{
+}
