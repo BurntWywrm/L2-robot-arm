69d9ecb1f9c68b0a123d8386e98da050d4830fb1
Burnt <227668109+BurntWywrm@users.noreply.github.com>
2025-10-06 14:31:57 +1300

    Started robot_arm_alpha.ino

---
+++ scripts/robot_arm/robot_arm_alpha/robot_arm_alpha.ino
@@ -0,0 +1,18 @@
+// ESP32
+// Robot Arm Script ALPHA
+// https://github.com/BurntWywrm/L2-robot-arm
+
+/*
+ALPHA: Tests if the Actuators are properly 
+rotating according to user input (serial monitor).
+With a minimum of 0 degrees to a maximum of 180 degrees.
+*/
+
+void setup()
+{
+    Serial.begin(115200); // Starts serial monitor with a baud rate of 115200
+}
+
+void loop()
+{
+}
