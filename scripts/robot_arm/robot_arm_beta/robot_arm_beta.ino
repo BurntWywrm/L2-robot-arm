f7599b0663f92cc99c21bbb3823e1a773c52eb36
BURNSIDE HIGH SCHOOL <burntwyrm.work@gmail.com>
2025-10-07 13:25:50 +1300

    Update robot_arm_beta.ino
    Added description
    Added simple setup and void sketch

---
+++ scripts/robot_arm/robot_arm_beta/robot_arm_beta.ino
@@ -0,0 +1,17 @@
+// ESP32
+// Robot Arm Script ALPHA
+// https://github.com/BurntWywrm/L2-robot-arm
+
+/*
+ALPHA: Tests the ESP-NOW protocol so that
+it can recieve information from the controller.
+*/
+
+void setup()
+{
+    Serial.begin(115200); // Starts serial monitor with a baud rate of 115200
+}
+
+void loop()
+{
+}
